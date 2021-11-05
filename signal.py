846c8a6ab9ec6a82ea1133ab62656c992384e039
aaron.hoffer <github@hoffer.org>
2021-11-04 22:07:58 -0700

    Ready for another round of debugging

--- signal.py
+++ signal.py
@@ -8,18 +8,20 @@ class Signal:
         self.green_pin = green_pin
         io.setup(self.red_pin, io.OUT)
         io.setup(self.green_pin, io.OUT)
-        self.pwm = {self.green_pin: io.PWM(self.green_pin, 10), self.red_pin : io.PWM(self.red_pin, 10)}
-        self.fast_green = io.PWM(self.green_pin, 10)
-        self.fast_red = io.PWM(self.red_pin, 10)
+        self.green_pwm = io.PWM(self.green_pin, 10)
+        self.red_pwm = io.PWM(self.red_pin, 10)
 
     def _set(self, green, red):
         io.output(self.green_pin, green)
         io.output(self.red_pin, red)
 
     def fast_blink(self, blocked):
-        modulator = self.pwm[self.red_pin] if blocked else self.pwm[self.green_pin]
-        modulator.ChangeFrequency(10)
-        modulator.start(50)
+        if blocked:
+            self.red_pwm.ChangeFrequency(50)
+            self.start(50)
+        else:
+            self.green_pwm.ChangeFrequency(50)
+            self.start(50)
 
     def show(self, blocked):
         # If blocked, turn off green and turn on red
@@ -35,17 +37,15 @@ class Signal:
         self._set(False, True)
 
     def fast_green(self):
-        self.fast_green(50)
+        self.green_pwm(50)
 
     def fast_red(self):
-        self.fast_red(50)
+        self.red_pwm(50)
 
     def alternate(self):
-        # LED alternate every half second.
-        red = self.pwm[self.red_pin]
-        green = self.pwm[self.green_pin]
-        red.ChangeFrequency(1)
-        green.ChangeFrequency(1)
-        red.start(50)
+        # Alternate LED every half second.
+        self.pwm_red.ChangeFrequency(1)
+        self.pwm_red.start(50)
         time.sleep(0.5)
-        green.start(50)
+        self.pwm_green.ChangeFrequency(1)
+        self.pwm_red.start(50)
