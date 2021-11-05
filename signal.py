1b8658192f1fad1af435c2206b53a7d73359b2f1
aaron.hoffer <github@hoffer.org>
2021-11-05 10:35:01 -0700

    Ready for another round of debugging

--- signal.py
+++ signal.py
@@ -4,7 +4,6 @@ import RPi.GPIO as io
 
 io.setwarnings(False)
 
-
 class Signal:
     def __init__(self, red_pin, green_pin):
         io.setmode(io.BCM)
@@ -16,41 +15,41 @@ class Signal:
         self.red_pwm = io.PWM(self.red_pin, 1)
 
 
-def _set(self, pwm, duty_cycle=100, hertz=1):
-    pwm.ChangeFrequency(hertz)
-    pwm.start(duty_cycle)
+    def _set(self, pwm, duty_cycle=100, hertz=1):
+        pwm.ChangeFrequency(hertz)
+        pwm.start(duty_cycle)
 
 
-def _green_off(self):
-    self._set(self.green_pwm, 0)
+    def _green_off(self):
+        self._set(self.green_pwm, 0)
 
 
-def _red_off(self):
-    self._set(self.red_pwm, 0)
+    def _red_off(self):
+        self._set(self.red_pwm, 0)
 
 
-def blink(self):
-    # Alternate LED every half second.
-    self._set(self.red_pwm, 50)
-    time.sleep(0.5)
-    self._set(self.green_pwm, 50)
+    def blink(self):
+        # Alternate LED every half second.
+        self._set(self.red_pwm, 50)
+        time.sleep(0.5)
+        self._set(self.green_pwm, 50)
 
 
-def solid_green(self):
-    _red_off(self)
-    _set(self.green_pwm, 100)
+    def solid_green(self):
+        self._red_off(self)
+        self._set(self.green_pwm, 100)
 
 
-def solid_red(self):
-    _green_off(self)
-    _set(self.red_pwm, 100)
+    def solid_red(self):
+        self._green_off(self)
+        self._set(self.red_pwm, 100)
 
 
-def fast_green(self):
-    _red_off()
-    _set(self.green_pwm, 50, 50)
+    def fast_green(self):
+        self._red_off()
+        self._set(self.green_pwm, 50, 50)
 
 
-def fast_red(self):
-    _green_off()
-    _set(self.red_pwm, 50, 50)
+    def fast_red(self):
+        self._green_off()
+        self._set(self.red_pwm, 50, 50)
